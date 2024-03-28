c53c2359f4df98f16a76a22c866ff2dc5569e809
Nikhil J <nikhilsrisai1422@gmail.com>
2024-03-28 13:27:04 +0530

    ramana load vachindhe

---
+++ scripts/items.js
@@ -0,0 +1,5 @@
+const items = [
+    {title: "name displays on page", tag: ["all"], img: "media\image1.jpg"},
+    {title: "name displays on page", tag: ["mix"], img: "media\image6.jpg"},
+    {title: "name displays on page", tag: ["design"], img: "media\image4.jpg"}
+];
