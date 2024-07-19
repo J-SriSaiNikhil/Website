ee67220184dc5c11d2733cba4352e76618172082
Nikhil J <nikhilsrisai1422@gmail.com>
2024-07-19 18:12:32 +0530

    pedha update idhe

--- scripts/workScript.js
+++ scripts/workScript.js
@@ -1,3 +1,53 @@
+
+document.addEventListener("DOMContentLoaded",function(){
+    const activeItemIndicator = CSSRulePlugin.getRule(".menuitem p#active::after");
+    const toggleBotton = document.querySelector(".menuBtn");
+    const toggleBotton1 = document.querySelector(".homebtn");
+    let isOpen = false;
+    
+    gsap.set(".menuitem p", { y: 255 });
+    
+    const timeline = gsap.timeline({ paused: true });
+    
+    timeline.to(".overlay",{
+        duration: 1.5,
+        clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        ease: "power4.inOut"
+    });
+    
+    timeline.to(".menuitem p",{
+        duration: 1.5,
+        y: 0,
+        stagger:0.2,
+        ease: " power4.Out"
+    },"-=1");
+    
+    timeline.to(activeItemIndicator,{
+        width:"100%",
+        duration: 1,
+        ease:"power4.Out",
+        delay:0.5
+    },"<");
+    
+    timeline.to(".subnav",{
+        bottom: "10%",
+        opacity: 1,
+        duration: 0.5,
+        delay: 0.5
+    },"<");
+    
+    toggleBotton.addEventListener("click",function(){
+        if(isOpen){
+            timeline.reverse();
+        }else{  
+            timeline.play();
+        }
+        isOpen = !isOpen;
+    });
+    
+    });
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const itemsContainer = document.querySelector(".items");
     const itemscols = document.querySelectorAll(".items-col");
@@ -91,3 +141,29 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+
+{/* <div class="container">
+<div class="filters">
+    <div class="filter active" data-filter="all">
+        <p>(34)</p>
+        <h1>All</h1>
+    </div>
+    <div class="filter" data-filter="mix">
+        <p>(12)</p>
+        <h1>Photography</h1>
+    </div>
+    <div class="filter" data-filter="design">
+        <p>(12)</p>
+        <h1>Projects</h1>
+    </div>
+    <div class="filter" data-filter="production">
+        <p>(10)</p>
+        <h1>Production</h1>
+    </div>
+</div>
+<div class="items">
+    <div class="items-col"></div>
+    <div class="items-col"></div>
+</div>
+</div> */}
